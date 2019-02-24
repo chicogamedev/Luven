7ceea35d81a7e5402d63d97a9498ae78d4a9ea81
Lionel Mac <leeser.lionel@gmail.com>
2019-02-24 09:17:06 +0100

    Hum...
    Now I have 3 fps on my macbook Air ... x) I think that's not a good idea hahaha

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -14,8 +14,10 @@ extern vec2 screen;
 
 extern mat4 viewMatrix;
 
+extern int lightsCount;
+
 vec4 position(mat4 transform_projection, vec4 vertex_position) {
-    for (int i = 0; i < NUM_LIGHTS; i++) {
+    for (int i = 0; i < lightsCount; i++) {
         lightsPositions[i] = (viewMatrix * vec4(lights[i].position, 0.0, 1.0)).xy / screen;
     }
 
